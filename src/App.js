1614c0013597113c9021d43c055a55094c7c6697
Tuzbay <t.uzbayyelce@gmail.com>
2023-02-27 18:04:48 +0300

    First Template Created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div>
+      <h1>Disney+ Clone Project Login Page</h1>
+    </div>
+  );
+}
+
+export default Login;
