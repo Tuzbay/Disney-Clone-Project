af4db597d29aebec33023c01b3ead7e3d7f23014
Tuzbay <t.uzbayyelce@gmail.com>
2023-03-01 17:41:15 +0300

    Header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login/Login";
+import Login from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzUmqIkwDy1RzarGshhIzflTX78Py-BHs",
+  authDomain: "disneyplus-clone-b4271.firebaseapp.com",
+  projectId: "disneyplus-clone-b4271",
+  storageBucket: "disneyplus-clone-b4271.appspot.com",
+  messagingSenderId: "112811671796",
+  appId: "1:112811671796:web:9311e32e42ce8d96231ea1",
+  measurementId: "G-RBPRR59WRV",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
