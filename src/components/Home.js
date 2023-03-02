4ed96e44dbb670c7d1a7caeb524e416db7ac7cfe
Tuzbay <t.uzbayyelce@gmail.com>
2023-03-03 00:52:48 +0300

    Carousel added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <ImgSlider />
+    </Container>
+  );
 }
 
 export default Home;
+
+const Container = styled.div`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-y: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
