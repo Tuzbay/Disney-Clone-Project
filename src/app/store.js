94bd2bb1b42ff39323c67c16504392112337f43e
Tuzbay <t.uzbayyelce@gmail.com>
2023-03-05 12:23:51 +0300

    Firebase&Home Page added

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import movieReducer from "../features/user/userSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   },
 });
